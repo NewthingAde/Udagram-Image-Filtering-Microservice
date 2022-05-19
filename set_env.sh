f4556ac55a7e9ebfb447c03be640becd44735ab0
Oluwasegun <80678596+NewthingAde@users.noreply.github.com>
2022-05-19 14:11:15 +0200

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=newthing
+export POSTGRES_PASSWORD=newthing
+export POSTGRES_HOST=newthing.comb4hoh2nqs.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::microservice-12345-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
