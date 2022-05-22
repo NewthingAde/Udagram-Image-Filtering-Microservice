b4294c4e6a82d7e40eec207ddcb662e0d8078dac
Oluwasegun <80678596+NewthingAde@users.noreply.github.com>
2022-05-23 01:59:17 +0200

    adding deployment folder

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a53f9acaff1cf45e1bc43daf8352226c-2066641596.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
